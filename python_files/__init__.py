fa621a510e1dddf1aaa8e22fca69025c14b6236e
Zach Reid <75558285+zachreid-96@users.noreply.github.com>
2024-11-23 20:52:31 -0600

    Added two solutions for Google Foobar Challenge 'Binary Bunnies', both in binary_bunnies.py.
    solution(seq) uses a recursive binomial coefficient equation at each level of the binary tree and multiplies up.
    
    brute_force(seq) finds all permutations of seq[1:] and recursively checks if each layer (top down) is the same and stops if they are not the same. So if layer 3 results are different it will not continue to layer 4, 5, 6, etc.

---
+++ python_files/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ["binary_bunnies"]
+
+'''
+Included to make importing to a main.py for testing easier.
+'''
+
+from . import binary_bunnies
---
+++ python_files/binary_bunnies.py
@@ -0,0 +1,132 @@
+import math
+from itertools import permutations
+
+'''
+Description: Recursively continues the party from brute_force
+    recursively checks if passed permutation will give the same output as the passed original order
+    this is a 'smart brute force' method, smart because it checks until it fails
+    checks layer by layer (doesn't check depth 7 if depth 3 fails)
+    will get 'head' of each list and compare them
+        if EQUAL - calls itself with the lists split into their right and left trees
+        if NOT-EQUAL - returns False
+            
+    true_order = [2, 3, 1] && perm = [2, 1, 3]
+    true_head = 2 && perm_head = 2 -- EQUAL SO
+    true_head_left, perm_head_left = [1] && true_head_right, perm_head_right = [3]
+    calls itself again
+        
+    true_order = [2, 1, 3] && perm = [3, 2, 1]
+    true_head = 2 && perm_head = [3] -- NOT-EQUAL SO
+    return False
+        
+Args:
+    true_order: a list of numbers [4, 3, 1, 2]; the original list of numbers passed
+    perm: a list of numbers [3, 2, 1, 4]; the permutation of the original list
+Returns:
+    True if the entire order is preserved and will result in same tree
+    False if the order is not preserved and will not result in same tree
+'''
+
+
+def brute_force_solve(true_order, perm):
+    if true_order == []:
+        return True
+
+    if perm == []:
+        return True
+
+    true_head = true_order[0]
+    true_remainder = true_order[1:]
+
+    perm_head = perm[0]
+    perm_remainder = perm[1:]
+
+    if true_head == perm_head:
+
+        true_head_left = [true_remainder[i] for i in range(len(true_remainder)) if true_remainder[i] < true_head]
+        true_head_right = [true_remainder[i] for i in range(len(true_remainder)) if true_remainder[i] > true_head]
+
+        perm_head_left = [perm_remainder[i] for i in range(len(perm_remainder)) if perm_remainder[i] < perm_head]
+        perm_head_right = [perm_remainder[i] for i in range(len(perm_remainder)) if perm_remainder[i] > perm_head]
+
+        if brute_force_solve(true_head_left, perm_head_left) and brute_force_solve(true_head_right, perm_head_right):
+            return True
+
+    return False
+
+
+'''
+Description: Wrapper for brute_force_solve (gets the party started)
+    takes the original sequence, finds the tree head and the tree remainder
+    finds the tree left && finds the tree right (based on tree_head)
+    gets all permutations of the remainder and loops them
+    in LOOP finds perm tree left and perm tree right
+    checks if perm tree left and perm tree right will result in same as true tree left and true tree right
+Args:
+    seq: the passed sequence and will need to find all permutations of said sequence that result in same tree
+Returns:
+    returns the integer of how many permutations result in the same tree formation
+'''
+
+
+def brute_force(seq):
+    matches_found = 0
+
+    start_head = seq[0]
+    remainder = seq[1:]
+
+    true_left = [remainder[i] for i in range(len(remainder)) if remainder[i] < start_head]
+    true_right = [remainder[i] for i in range(len(remainder)) if remainder[i] > start_head]
+
+    perms = list(permutations(remainder))
+
+    for perm in perms:
+
+        left_perm = [perm[i] for i in range(len(perm)) if perm[i] < start_head]
+        right_perm = [perm[i] for i in range(len(perm)) if perm[i] > start_head]
+
+        if brute_force_solve(true_left, left_perm) and brute_force_solve(true_right, right_perm):
+            matches_found += 1
+
+    return matches_found
+
+
+'''
+Description: 
+    Recursively finds the binomial coefficient for a given set of numbers and multiplies up the tree
+Args:
+    seq: the passed set of numbers
+Returns:
+    returns the number of permutations that result in the same tree
+'''
+
+
+def binomial_coefficient(seq):
+    if len(seq) <= 2:
+        return 1
+
+    head = seq[0]
+    left = [seq[i] for i in range(len(seq)) if seq[i] < head]
+    right = [seq[i] for i in range(len(seq)) if seq[i] > head]
+
+    current_coefficient = (math.factorial(len(left) + len(right)) /
+                           (math.factorial(len(left)) * math.factorial(len(right))))
+    left_coefficient = binomial_coefficient(left)
+    right_coefficient = binomial_coefficient(right)
+
+    return current_coefficient * left_coefficient * right_coefficient
+
+
+'''
+Description: 
+    wrapper for binomial_coefficient(seq)
+    named solution as per the google challenge description in ../text_files/binary_bunnies.txt
+Args:
+    seq: the passed set of numbers
+Returns:
+    returns integer value of permutations of seq that will create same tree
+'''
+
+
+def solution(seq):
+    return int(binomial_coefficient(seq))
